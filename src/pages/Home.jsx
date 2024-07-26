5a67511682ef168ce73eb37d9fc880cfb4396790
ha0goo <likethat0923@gmail.com>
2024-07-26 17:37:45 +0900

    뮤직스타일

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
-import Developer from '../components/contents/Developer'
+import Music from '../components/contents/Music'
 import Webd from '../components/contents/Webd'
 import Website from '../components/contents/Website'
 import Gsap from '../components/contents/Gsap'
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <Main title="마이 페이지" description="마이 페이지입니다.">
       <Today/>
-      <Developer/>
+      <Music/>
       <Webd/>
       <Website/>
       <Gsap/>
